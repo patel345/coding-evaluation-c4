eda3e62d4bd6c1acb7f9fe688269b7fdedfa3bd0
patel345 <108001009+patel345@users.noreply.github.com>
2022-07-18 16:33:27 +0530

    Add files via upload

---
+++ admitted.js
@@ -0,0 +1,19 @@
+admitted=JSON.parse(localStorage.getItem("admitted"));
+    admitted.forEach(function(elem){
+let tr=document.createElement("tr");
+  let td1=document.createElement("td");
+  td1.innerText=elem.personName;
+  let td2=document.createElement("td");
+  td2.innerText=elem.personAge;
+  let td3=document.createElement("td");
+  td3.innerText=elem.personGender;
+  let td4=document.createElement("td");
+  td4.innerText=elem.personEmail;
+  let td5=document.createElement("td");
+  td5.innerText=elem.personPhoneNo;
+ 
+  tr.append(td1,td2,td3,td4,td5);
+  document.querySelector("tbody").append(tr);
+
+
+});
